d312b3653b672bdc7b8592dc31a1048bc98b379c
adurga-ariqt <adurga@ariqt.com>
2022-11-05 15:56:13 +0530

    Completed

--- Test1_bhavani_Electricitybill/Program.cs
+++ Test1_bhavani_Electricitybill/Program.cs
@@ -4,3 +4,13 @@ using Test1_bhavani_Electricitybill;
 Console.WriteLine("Hello, World!");
 ElectricityBill objElectricityBill = new ElectricityBill();
 objElectricityBill.ElectricityBillMethod();
+Console.WriteLine("Enter customer name:");
+String Cname = Console.ReadLine();
+Console.WriteLine("Enter number:");
+ int Meternumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Enter Units:");
+objElectricityBill.Units=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("your service type is domestic enter 'd' or 'D' \n" + "your service type is commertial enter 'C' or 'c '");
+
+objElectricityBill.ServiceType = Console.ReadLine();
+objElectricityBill.ElectricityBillMethod();
